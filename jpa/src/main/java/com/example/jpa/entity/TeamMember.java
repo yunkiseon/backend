411e1542eddc13c2c062ed1ea484d93897362537
yunkiseon <yks97016033@gmail.com>
2025-12-04 18:14:26 +0900

    추가

--- jpa/src/main/java/com/example/jpa/entity/TeamMember.java
+++ jpa/src/main/java/com/example/jpa/entity/TeamMember.java
@@ -27,9 +27,8 @@ public class TeamMember {
     @ManyToOne(optional = false)
     @JoinColumn(name = "team_id")
     private Team team;
-    // joincolumn 은 어느 컬럼과 조인할 것인지 지정하는 것
     //(name = "team_id") 이 부분이 oracle 에선 필수이다. 테이블명_기본키명
     // 만약 텀을 두고 만들면 update로 해두었다면 team_id, team_team_id 두 개가 된다.
-    
+
 
 }
--- jpa/src/test/java/com/example/jpa/repository/TeamRepositoryTest.java
+++ jpa/src/test/java/com/example/jpa/repository/TeamRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.jpa.entity.Team;
 import com.example.jpa.entity.TeamMember;
 
 @SpringBootTest
@@ -15,9 +16,15 @@ public class TeamRepositoryTest {
     @Autowired
     private TeamMemberRepository teamMemberRepository;
 
+
+
     @Test
     public void insertTest(){
-        TeamMember member = TeamMember.builder().name("홍길동").build();
+
+        Team team = Team.builder().name("team1").build();
+        teamRepository.save(team);
+
+        TeamMember member = TeamMember.builder().name("홍길동").team(team).build();
         teamMemberRepository.save(member);
     }
     
